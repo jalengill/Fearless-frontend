81c60afefbef263cc042a5d60dc1140fbc9b6038
Jalen Gill <jalen.gill@macaulay.cuny.edu>
2022-11-30 15:20:05 -0500

    Moving to ubuntu

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -15,6 +15,11 @@ class AttendeeListEncoder(ModelEncoder):
     model = Attendee
     properties = ["name"]
 
+    def get_extra_data(self, o):
+        return {
+            "conference": o.conference.name,
+        }
+
 
 class AttendeeDetailEncoder(ModelEncoder):
     model = Attendee
